e01728a7ab24f755d7c60d908462dce3ca81b881
jketch836 <jketch@JoshKs-MacBook-Pro.local>
2014-03-04 14:47:21 -0500

    avf_lab1_p3
    avf_lab1_p3

--- avf_marvelAPI_lab1/Resources/app.js
+++ avf_marvelAPI_lab1/Resources/app.js
@@ -1,3 +1,6 @@
+//requiring app.js
+var api = require('marvel_api');
+
 //Window Start
 var win = Ti.UI.createWindow({
 	title : 'Marvel Comics',
@@ -5,9 +8,15 @@ var win = Ti.UI.createWindow({
 });
 //Window End
 
+//Window Start
+var navWin = Ti.UI.iOS.createNavigationWindow({
+	window : win
+});
+//Window End
+
 //marvel logo picture start
 var logo = Ti.UI.createImageView({
-	top : '63dp',
+	top : '43dp',
 	height : '100%',
 	width : '100%',
 	image : 'marvel-logo-wallpaper.jpg'
@@ -24,84 +33,110 @@ var searchbar = Ti.UI.createSearchBar({
 
 //Table Start
 var comicTable = Titanium.UI.createTableView({
-	top : '20dp',
 	search : searchbar,
 	font : {
 		fontStyle : 'Helvetica',
 		fontSize : 14
 	},
-	opacity : .8
+	opacity : .9
 });
-// console.log(comicTable);
-//export table to marvel_api
-exports.comicTable = comicTable;
 //Table End
 
 comicTable.addEventListener('click', function(a) {
+
 	//calling vraibles from api start
+
 	var name, issue, desc, isbn, price;
 
-	title = a.rowData.title;
+	name = a.rowData.name;
 	issue = a.rowData.issue;
 	desc = a.rowData.desc;
-	isbn = a.rowData.isbn;
-	price = a.rowData.price;
+	page = a.rowData.page;
+	// price = a.rowData.price;
+	// thumbnail = a.rowData.thumbnail;
+
 	//calling vraibles from api end
 
 	//Window Start
 	var evtWin = Ti.UI.createWindow({
-		title : title,
-		backgroundColor : '#fff'
+		title : name,
+		backgroundColor : '#fff',
+		nav : navWin
 	});
 	//Window End
 
 	//marvel logo picture start
 	var evtLogo = Ti.UI.createImageView({
-		top : '63dp',
 		height : '100%',
 		width : '100%',
 		image : 'marvel-logo-wallpaper.jpg',
-		opacity : .2
+		// image : thumbnail,
+		opacity : .1
 	});
 	//marvel logo picture end
 
 	//Labels Begin
 	var titleView = Ti.UI.createLabel({
-		top : '15%',
-		left : '15%',
-		text : 'Title: ' + title
+		top : '10%',
+		left : '10%',
+		text : 'Title: ' + name,
+		font : {
+			fontStyle : 'Helvetica',
+			fontSize : 22
+		},
 	});
 
 	var issueView = Ti.UI.createLabel({
-		top : '17%',
-		left : '15%',
-		text : 'Issue #: ' + issue
-	});
-
-	var descView = Ti.UI.createLabel({
-		top : '19%',
-		left : '15%',
-		text : 'Description: ' + desc
+		top : '15%',
+		left : '10%',
+		text : 'Issue #: ' + issue,
+		font : {
+			fontStyle : 'Helvetica',
+			fontSize : 22
+		},
 	});
 
 	var isbnView = Ti.UI.createLabel({
-		top : '23%',
-		left : '15%',
-		text : 'ISBN #: ' + isbn
+		top : '18%',
+		left : '10%',
+		text : '# of Pages: ' + page,
+		font : {
+			fontStyle : 'Helvetica',
+			fontSize : 22
+		},
+	});
+
+	var descView = Ti.UI.createLabel({
+		top : '25%',
+		left : '10%',
+		right : '15%',
+		text : 'Description: ' + desc,
+		font : {
+			fontStyle : 'Helvetica',
+			fontSize : 22
+		},
 	});
 
 	var priceView = Ti.UI.createLabel({
-		top : '15%',
+		top : '10%',
 		right : '15%',
-		text : 'Price: ' + price
+		text : 'Price: ' + price,
+		font : {
+			fontStyle : 'Helvetica',
+			fontSize : 22
+		},
 	});
 	//Labels End
-	
+
 	//EventListener Main Code
 	evtWin.add(evtLogo, titleView, issueView, descView, isbnView, priceView);
-	evtWin.open();
+	// evtWin.open();
+	navWin.openWindow(evtWin, {
+		animate : true
+	});
 });
 
 //Main Code
 win.add(logo, comicTable);
-win.open();
+// win.open();
+navWin.open();
