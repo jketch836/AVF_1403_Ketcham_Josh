7e5ee856c2997ad0666adec4fa953b864c366549
jketch836 <jketch@JoshKs-MacBook-Pro.local>
2014-03-05 23:44:36 -0500

    AVF_LAB2_GEOLOCATION_P1
    avf.lab2 geolocation p1

--- avf_lab2_location/Resources/app.js
+++ avf_lab2_location/Resources/app.js
@@ -1,96 +1,139 @@
 // //require api info
-// var geo = require('geo_api');
+var geo = require('geo_api');
 
 //create window
 var win = Ti.UI.createWindow({
-	
+	title : 'NY Times Stories',
+	backgroundColor : '#fff'
 });
 
-    var characterScroll = Ti.UI.createScrollView({
-		layout: 'vertical',
-		height: '530dp',
-		width: 320,
-	    top: 40,
-	    showVerticalScrollIndicator:true
-	});
+//Scroll View Start
+var timesScroll = Ti.UI.createScrollView({
+	layout : 'vertical',
+	height : '94.5%',
+	width : '100%',
+	top : '4%',
+	showVerticalScrollIndicator : true
+});
+//Scroll View End
 
-	nytimesListTemplate = {
-		properties:
-			{
-			top: 20,
-			height: 74	
+//listview template start
+timesTemplate = {
+	properties : {
+		top : '1%',
+		height : '7%'
+	},
+	childTemplates : [{
+		type : "Ti.UI.Label",
+		bindId : 'name',
+		properties : {
+			color : "black",
+			font : {
+				fontSize : 16,
+				fontFamily : "Arial"
 			},
-		childTemplates:
-		[
-			{
-				type: "Ti.UI.ImageView",
-				bindId: 'thumbnail',
-				properties:
-				{
-					width: 74,
-					height: 74,
-					left: 0,
-					top:0
-				}
+			left : '7%',
+			top : '1%'
+		}
+	}, {
+		type : "Ti.UI.Label",
+		bindId : 'country',
+		properties : {
+			color : "black",
+			font : {
+				fontSize : 12,
+				fontFamily : "Arial"
 			},
-			{
-				type: "Ti.UI.Label",
-				bindId: 'name',
-				properties:
-				{
-					color: "black",
-					font: {fontSize: 18, fontFamily: "Arial", fontWeight: "bold"},
-					left: 85,
-					top: 5
-				}
-			},
-			{
-				type: "Ti.UI.Label",
-				bindId: 'race',
-				properties:
-				{
-					color: "grey",
-					font: {fontSize: 14, fontFamily: "Arial"},
-					left: 85,
-					top: 40
-				}
-			},
-			{
-				type: "Ti.UI.Label",
-				bindId: 'division',
-				properties:
-				{
-					color: "black",
-					font: {fontSize: 14, fontFamily: "Arial"},
-					left: 200,
-					top: 40
-				},
-			},
-			{
-				type: "Ti.UI.Label",
-				bindId: 'level',
-				properties:
-				{
-					color: "grey",
-					font: {fontSize: 14, fontFamily: "Arial"},
-					right: 10,
-					top: 10
-				}
-			}	
-		]
-	};
-	
-	//API Object Info
-	var timesList = Ti.UI.createListSection({
-	});
-	var timesListView = Ti.UI.createListView({	
+			right : '10%',
+			top : '1%'
+		},
+	}]
+};
+//listview template end
+
+//listview start
+var listview = Ti.UI.createListView({
 	//	search: search,
-		top:20,
-		// height: '500dp',
-		templates:{'defaultTemplate': nytimesListTemplate},
-		defaultItemTemplate: 'defaultTemplate'
-		});
+	top : '0%',
+	// height: '500dp',
+	templates : {
+		'defaultTemplate' : timesTemplate
+	},
+	defaultItemTemplate : 'defaultTemplate'
+});
+//listview end
 
+//evt listener start
+listview.addEventListener('itemclick', function(e) {
+
+
+
+
+	//calling vraibles from geo_api start
+// 
+	// var name, issue, desc, isbn, cost, thumbnail1;
+	//
+	// name = e.section.itemIndex.name;
+	// issue = a.rowData.issue;
+	// desc = a.rowData.desc;
+	// page = a.rowData.page;
+	// cost = a.rowData.cost;
+	// thumbnail1 = a.rowData.thumbnail1;
+// 
+	//calling vraibles from geo_api end
+// 
+	// //create window
+	// var evtWin = Ti.UI.createWindow({
+		// title : 'NY Times Location',
+		// backgroundColor : '#fff'
+	// });
+// 
+	// //back button start
+	// var backView = Ti.UI.createView({
+		// backgroundColor : "#333",
+		// borderRadius : '3%',
+		// height : '7%',
+		// width : '20%',
+		// top : '3%',
+		// left : '0%',
+		// file : 'app.js'
+	// });
+// 
+	// var backLabel = Ti.UI.createLabel({
+		// text : "Back",
+		// font : {
+			// fontSize : 16,
+			// fontFamily : "Helvetica",
+			// fontWeight : "bold"
+		// },
+		// color : "#fff",
+		// center : 0,
+		// file : 'app.js'
+	// });
+	// //back button end
+// 
+	// //city name start
+	// var city = Ti.UI.createLabel({
+		// text : name,
+		// top : '20%',
+		// font : {
+			// fontSize : 16,
+			// fontFamily : "Helvetica",
+			// fontWeight : "bold"
+		// },
+		// color : "#000",
+	// });
+	// //city name end
+
+	//evt Main Code
+	backView.add(backLabel);
+	// evtWin.add(backView, city);
+	evtWin.open();
+});
+//evt listener end
 
 //Main Code
+timesScroll.add(listview);
+win.add(timesScroll);
 win.open();
+
