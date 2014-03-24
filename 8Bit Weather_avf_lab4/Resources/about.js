007beefb5ce1e48d2e60b151a1fc558288a9ec8d
jketch836 <jketch@JoshKs-MacBook-Pro.local>
2014-03-24 12:15:46 -0400

    AVF_LAB4_P5
    weather app

---
+++ 8Bit Weather_avf_lab4/Resources/about.js
@@ -0,0 +1,40 @@
+// require JS files
+var ap = require('app');
+
+var aWin = Ti.UI.createWindow({
+	center : '0%',
+	height : '35%',
+	width : '35%',
+	backgroundColor : '#fff',
+	borderRadius : '7%'
+});
+
+var close = Ti.UI.createLabel({
+	top : '3%',
+	left : '3%',
+	text : 'Close',
+		font : {
+		fontStyle : 'Helvetica',
+		fontSize : '18dp'
+	},
+	color : '#0000CD'
+});
+
+close.addEventListener('click', function(){
+	aWin.close();
+});
+
+var infoLBL = Ti.UI.createLabel({
+	center : '0%',
+	left : '10%',
+	right : '10%',
+	text : 'This app was inspired by a many other weather apps. It shows the current weather where you are, as well as the humidity and 7-Day forecast. I hope you enjoy it!',
+	font : {
+		fontStyle : 'Helvetica',
+		fontSize : '22dp'
+	},
+	color : '#000'
+});
+
+aWin.add(close, infoLBL);
+aWin.open();
